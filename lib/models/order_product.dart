bb97eeb33a0ea28605ebc4d0138489a8c0b08f8e
frederikpyt <frederikpyt@protonmail.com>
2022-06-02 22:26:55 +0200

    #92 - Refactored code into a new widget

--- lib/models/order_product.dart
+++ lib/models/order_product.dart
@@ -1,3 +1,5 @@
+import 'package:restaurant_app_flutter/models/product.dart';
+
 class OrderProduct {
   int id;
   double priceAtPurchase;
@@ -6,15 +8,20 @@ class OrderProduct {
   int orderId;
   String createdAt;
   String updatedAt;
+  Product product;
 
-  OrderProduct(this.id, this.priceAtPurchase, this.status, this.productId, this.orderId, this.createdAt, this.updatedAt);
+  OrderProduct(
+      this.id, this.priceAtPurchase, this.status, this.productId, this.orderId, this.createdAt, this.updatedAt, this.product);
 
+  //TODO: Fix so these are not the product id, createdAt, updatedAt, but the orderProduct (Change endpoint)
+  //TODO: Remove the in_progress status
   OrderProduct.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        priceAtPurchase = json['price_at_purchase'] + .0,
-        status = json['status'],
-        productId = json['product_id'],
-        orderId = json['order_id'],
-        createdAt = json['created_at'],
-        updatedAt = json['updated_at'];
+      : id = json['pivot']['id'] ?? 0,
+        priceAtPurchase = json['pivot']['price_at_purchase'] + .0,
+        status = json['pivot']['status'],
+        productId = json['pivot']['product_id'],
+        orderId = json['pivot']['order_id'],
+        createdAt = json['pivot']['created_at'] ?? '',
+        updatedAt = json['pivot']['updated_at'] ?? '',
+        product = Product.fromJson(json);
 }
--- lib/screens/kitchen.dart
+++ lib/screens/kitchen.dart
@@ -4,6 +4,7 @@ import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import 'package:restaurant_app_flutter/factories/bearer_token_factory.dart';
 import 'package:restaurant_app_flutter/factories/order_service_factory.dart';
 import 'package:restaurant_app_flutter/models/order.dart';
+import 'package:restaurant_app_flutter/models/order_product.dart';
 import 'package:restaurant_app_flutter/models/product.dart';
 import 'package:restaurant_app_flutter/screens/login.dart';
 import 'package:restaurant_app_flutter/services/order.dart';
@@ -23,116 +24,25 @@ class _KitchenPageState extends State<KitchenPage> {
     return await orderService.getOrders();
   }
 
-  List<Widget> getOrderWidgets(List<Order> orders) {
-    List<Widget> orderWidgets = <Widget>[];
-
-    for (var order in orders) {
-      orderWidgets.add(
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.red.shade900,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(16),
-            ),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        order.getHourMinutes(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      '#' + order.id.toString(),
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(16),
-                      bottomRight: Radius.circular(16),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      children: [...getProductTexts(order)],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-
-    return orderWidgets;
-  }
-
-  List<Widget> getProductTexts(Order order) {
-    Map<int, int> productIdToCountMap = {};
-
-    for (Product product in order.products) {
-      productIdToCountMap.update(product.id, (value) => value + 1, ifAbsent: () => 1);
-    }
-
-    List<Widget> textWidgets = [];
-
-    productIdToCountMap.forEach((key, value) {
-      textWidgets.add(
-        Container(
-          child: Row(
-            children: [
-              Expanded(
-                child: Text(
-                  value.toString() + 'x ' + order.products.where((product) => product.id == key).first.name,
-                  style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Transform.scale(
-                scale: 1.6,
-                child: Checkbox(
-                  value: true,
-                  onChanged: (value) {},
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-                  activeColor: Colors.red.shade800,
-                ),
-              ),
-            ],
-          ),
-          decoration: const BoxDecoration(border: Border(bottom: BorderSide(width: 1.1))),
-        ),
-      );
-    });
-
-    return textWidgets;
-  }
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Order>>(
       future: getPendingOrders(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          List<Widget> orderWidgets = getOrderWidgets(snapshot.data!);
+          List<Widget> orderWidgets = <Widget>[];
+
+          for (var order in snapshot.data!) {
+            orderWidgets.add(
+              KitchenOrder(
+                onDeletion: () {
+                  print('deleting...');
+                  setState(() {});
+                },
+                order: order,
+              ),
+            );
+          }
 
           return SafeArea(
             child: Scaffold(
@@ -142,7 +52,9 @@ class _KitchenPageState extends State<KitchenPage> {
               body: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: RefreshIndicator(
-                  onRefresh: () async => setState(() {}),
+                  onRefresh: () async => setState(() {
+                    print('refreshing');
+                  }),
                   child: GridView.builder(
                     gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
                       maxCrossAxisExtent: 400,
@@ -151,7 +63,9 @@ class _KitchenPageState extends State<KitchenPage> {
                     ),
                     itemCount: orderWidgets.length,
                     itemBuilder: (BuildContext ctx, index) {
-                      return orderWidgets[index];
+                      return StatefulBuilder(
+                        builder: (context, setState) => orderWidgets[index],
+                      );
                     },
                   ),
                 ),
@@ -182,5 +96,159 @@ class _KitchenPageState extends State<KitchenPage> {
         return const Center(child: CircularProgressIndicator());
       },
     );
+  }
+}
+
+class KitchenOrder extends StatefulWidget {
+  final Order order;
+  final Function onDeletion;
+
+  const KitchenOrder({Key? key, required this.onDeletion, required this.order}) : super(key: key);
+
+  @override
+  State<KitchenOrder> createState() => _KitchenOrderState();
+}
+
+class _KitchenOrderState extends State<KitchenOrder> {
+  @override
+  Widget build(BuildContext context) {
+    bool allChecked = isChecked(widget.order.orderProducts);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red.shade900,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16),
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    widget.order.getHourMinutes(),
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  '#' + widget.order.id.toString(),
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    ...getProductTexts(),
+                    if (allChecked)
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(primary: Colors.green),
+                            onPressed: () => widget.onDeletion(),
+                            icon: const Icon(Icons.check),
+                            label: const Text('Finish'),
+                          ),
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> getProductTexts() {
+    Map<int, int> productIdToCountMap = {};
+
+    for (Product product in widget.order.getProducts()) {
+      productIdToCountMap.update(product.id, (value) => value + 1, ifAbsent: () => 1);
+    }
+
+    List<Widget> textWidgets = [];
+
+    productIdToCountMap.forEach((productId, count) {
+      Product product = widget.order.getProducts().where((product) => product.id == productId).first;
+
+      List<OrderProduct> orderProducts =
+          widget.order.orderProducts.where((orderProduct) => orderProduct.productId == productId).toList();
+
+      bool checked = isChecked(orderProducts);
+
+      textWidgets.add(
+        Container(
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  count.toString() + 'x ' + product.name,
+                  style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Transform.scale(
+                scale: 1.6,
+                child: Checkbox(
+                  value: checked,
+                  onChanged: (value) {
+                    setState(() {
+                      for (var orderProduct in orderProducts) {
+                        if (value!) {
+                          orderProduct.status = 'deliverable';
+                        } else {
+                          orderProduct.status = 'ordered';
+                        }
+                      }
+                    });
+                  },
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                  activeColor: Colors.red.shade800,
+                ),
+              ),
+            ],
+          ),
+          decoration: const BoxDecoration(border: Border(bottom: BorderSide(width: 1.1))),
+        ),
+      );
+    });
+
+    return textWidgets;
+  }
+
+  bool isChecked(List<OrderProduct> orderProducts) {
+    bool checked = true;
+
+    for (var orderProduct in orderProducts) {
+      if (orderProduct.status == 'ordered') {
+        checked = false;
+        break;
+      }
+    }
+
+    return checked;
   }
 }
